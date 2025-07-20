66205b1e5887b1ce690aa1fd2fd651abe32c570f
fantastic-geek88 <radifanacademic@gmail.com>
2025-07-20 22:20:56 +0700

    Update dummy.py with new fruit and color

--- src/dummy.py
+++ src/dummy.py
@@ -3,5 +3,5 @@ import pandas as pd
 data = {"fruits": ["apple", "banana", "cherry"], "colors": ["red", "yellow", "purple"]}
 df = pd.DataFrame(data)
 
-# df.head(2)
+df.head(2)
 df.replace({"red": "green"}, inplace=True)
